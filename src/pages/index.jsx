29ae00094eda0575857a8493d02279fe67b00cc3
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-05-17 20:22:36 +0900

    ✨ counterを設置してuseCountでカウントする

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,22 +3,18 @@ import styles from "../styles/Home.module.css"
 import { Footer } from "../components/Footer"
 import { Main } from "../components/Main"
 import { Header } from "../components/Header"
-import { useCallback, useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export default function Home() {
-  const foo = 1
+  const [count, setCount] = useState(1)
 
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href)
-    e.preventDefault()
-    alert(foo)
-  }, [])
+  const handleClick = () => {
+    setCount((count) => count + 1)
+  }
 
   useEffect(() => {
-    console.log("mount")
     document.body.style.backgroundColor = "lightblue"
     return () => {
-      console.log("unmount")
       document.body.style.backgroundColor = ""
     }
   }, [])
@@ -30,9 +26,8 @@ export default function Home() {
       </Head>
 
       <Header />
-      <a href="/about" onClick={handleClick}>
-        ボタン
-      </a>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
       <Main page="index" />
       <Footer />
     </div>
